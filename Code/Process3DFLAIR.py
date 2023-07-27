ec2aeb998203e82091ee1ce946eae9f73307cdeb
Ela-Kan <e.ak.kanani@gmail.com>
2023-07-27 16:28:34 +0100

    Adding gradient map calculations

--- Code/Process3DFLAIR.py
+++ Code/Process3DFLAIR.py
@@ -540,11 +540,11 @@ class Process3DFLAIR():
             reorient.inputs.out_file = normalised_file_format.format(current_img_str)
             res = reorient.run()
 
-    def subtractImages(self, in_image_num, image_to_subtract_num, out_file, threshold = True):
+    def subtractImages(self, in_image_num, image_to_subtract_num, out_file, threshold = False):
         # input = time point flag for images e.g. 1 or 2
 
         # load in the images
-        normalised_file_format = self.subject_normalised_directory + '/nonlinear/' + self.subject_id+'_{}_D1.nii.gz'
+        normalised_file_format = self.subject_normalised_directory + self.subject_id+'_{}_D1.nii.gz'
         in_image_path = normalised_file_format.format(str(in_image_num).zfill(2))
         image_to_subtract_path = normalised_file_format.format(str(image_to_subtract_num).zfill(2))
 
@@ -575,9 +575,9 @@ class Process3DFLAIR():
         # Extract brain at final time point in sequence using HD-BET
 
         # Step 1) Extract brain using HD-BET and mask
-        self.extractBrain()
+        #self.extractBrain()
         # Step 2) Perform bias field correction
-        self.correctBiasField(method = 'FSL')
+        #self.correctBiasField(method = 'FSL')
         # Step 3) Perform intensity normalisation
         self.intensityNormalisation(useBiasCorrected = True)
         # Step 4: Compute subtraction map
@@ -620,9 +620,9 @@ class Process3DFLAIR():
         # Requires images to be in NIFTI already
 
         # Step 1) Extract brain using HD-BET and mask
-        #self.extractBrain()
+        self.extractBrain()
         # Step 2) Perform bias field correction
-        #self.correctBiasField(method = 'FSL')
+        self.correctBiasField(method = 'FSL')
         # Step 3) Perform intensity normalisation
         self.intensityNormalisation(useBiasCorrected = True)
         # Step 4: Compute variance map
@@ -631,6 +631,119 @@ class Process3DFLAIR():
         print('Completed variance map')
        
         return None
+    
+    def calcSlope(x1,x2,y1,y2):
+            
+            gradient = (y2 - y1) /  (x2-x1)
+            
+            return gradient
+    
+    
+    def calcGradientMaps(self, out_folder, days_between_scans):
+        
+        # load in all images that are normalised in the time series and their corresponding masks
+        normalised_file_format = self.subject_normalised_directory + '/rigid/' + self.subject_id+'_{}_D1.nii.gz' # only consider rigid registration here
+        brain_file_mask_format = self.subject_brain_directory + 'masks/'+ self.subject_id+'_{}_D1.nii'  
+        gradient_map_file_format = out_folder + self.subject_id+'_map_{}.nii.gz'  
+        image_paths = [] # initialise variables
+        mask_path = brain_file_mask_format.format(str(self.time_points_to_consider[-1]).zfill(2)) # the mask is always the last time point's BET
+
+        # create list of paths for masks and images
+        for i in range(len(self.time_points_to_consider)):
+            current_img_str = str(0)+str(self.time_points_to_consider[i]) # current time point string
+            image_paths.append(normalised_file_format.format(current_img_str))
+
+        # load in images for processing
+        images = [nib.load(image_path).get_fdata() for image_path in image_paths]         
+        # load in mask for processing
+        mask = nib.load(mask_path).get_fdata()   
+
+        # iterate through all points in the time series to create
+        # a list of tuples with the points to consider in each gradient map.
+        gradient_indices_list = []
+
+        for i in range (1,len(self.time_points_to_consider)):
+            print(i)
+            # calculate edge case
+            if i == 1 or i == len(self.time_points_to_consider) - 1:
+                gradient_indices_list.append([i,i+1])
+            else: 
+                gradient_indices_list.append([i-1, i, i+1])
+
+        # number of gradient maps
+        number_grad_maps = len(self.time_points_to_consider) - 1
+
+        # iterate through each gradient map:
+        for m in range(number_grad_maps):
+            map_num = m + 1 
+            # calculate the gradients at the edge cases
+            if map_num == 1: # if we are looking at the first map
+                # select the images
+                lower_bound_img = images[gradient_indices_list[m][0]-1] # minus one from all of these to follow python indexing
+                upper_bound_img = images[gradient_indices_list[m][1]-1]
+                lower_bound_days = days_between_scans[gradient_indices_list[m][0]-1]
+                upper_bound_days = days_between_scans[gradient_indices_list[m][1]-1]
+                gradient_map = np.zeros(images[m].shape) # preallocate gradient map image
+                # go through all points in the two images to compute the gradient
+                # iterate through each 'voxel' in the images
+                for i in range(gradient_map.shape[0]):
+                        for j in range(gradient_map.shape[1]):
+                                for k in range(gradient_map.shape[2]):
+                                        # if the mask says that there is no brain here, continue to next voxel
+                                        if mask[i,j,k] == 0:
+                                            continue
+                                        else:
+                                            gradient_map[i,j,k] = self.calcSlope(lower_bound_days,upper_bound_days,lower_bound_img[i,j,k],upper_bound_img[i,j,k])
+
+            elif map_num == number_grad_maps: # if we're looking at the last map
+                # select the images
+                lower_bound_img = images[gradient_indices_list[m][0]-1]
+                upper_bound_img = images[gradient_indices_list[m][1]-1]
+                lower_bound_days = days_between_scans[gradient_indices_list[m][0]-1]
+                upper_bound_days = days_between_scans[gradient_indices_list[m][1]-1]
+                gradient_map = np.zeros(images[m].shape) # preallocate gradient map image
+                # go through all points in the two images to compute the gradient
+                # iterate through each 'voxel' in the images
+                for i in range(gradient_map.shape[0]):
+                        for j in range(gradient_map.shape[1]):
+                                for k in range(gradient_map.shape[2]):
+                                        # if the mask says that there is no brain here, continue to next voxel
+                                        if mask[i,j,k] == 0:
+                                            continue
+                                        else:
+                                            gradient_map[i,j,k] = self.calcSlope(lower_bound_days,upper_bound_days,lower_bound_img[i,j,k],upper_bound_img[i,j,k])
+
+            else: # we aren't in the boundary cases, so use the polyfit function as we have 3 points
+                lower_bound_img = images[gradient_indices_list[m][0]-1]
+                mid_img = images[gradient_indices_list[m][1]-1]
+                upper_bound_img = images[gradient_indices_list[m][2]-1]
+                lower_bound_days = days_between_scans[gradient_indices_list[m][0]-1]
+                mid_num_days = days_between_scans[gradient_indices_list[m][1]-1]
+                upper_bound_days = days_between_scans[gradient_indices_list[m][2]-1]    
+                
+                gradient_map = np.zeros(images[m].shape) # preallocate gradient map image     
+             # go through all points in the two images to compute the gradient
+                # iterate through each 'voxel' in the images
+                for i in range(gradient_map.shape[0]):
+                        for j in range(gradient_map.shape[1]):
+                                for k in range(gradient_map.shape[2]):
+                                        # if the mask says that there is no brain here, continue to next voxel
+                                        if mask[i,j,k] == 0:
+                                            continue
+                                        else:
+                                            # set y 'array', i.e. intensities
+                                            y = [lower_bound_img, mid_img, upper_bound_img]
+                                            # set x 'array' i.e. time points in days
+                                            x = [lower_bound_days, mid_num_days, upper_bound_days]
+                                            # use polyfit to fit linear polynomial to points and extract the gradient
+                                            slope, intercept = np.polyfit(x,y,1)
+                                            gradient_map[i,j,k] = slope
+                
+            # save the gradient map for the current time point
+            grad_nifti = nib.Nifti1Image(gradient_map, affine=np.eye(4))
+            nib.save(grad_nifti, gradient_map_file_format.format(str(map_num)))
+            print('Computed map ' + str(map_num) + ' out of ' + str(number_grad_maps))
+            
 
 if __name__ == "__main__":
 
@@ -643,34 +756,43 @@ if __name__ == "__main__":
     test_total_num_time_points = subject_info_df.Time_Points[1] # auto use all from excel sheet
     
     # select the time points we want to consider in analysis
-    test_time_points_to_consider = [1,2,3,4,5]# for speed and testing the pipeline, only use the first two
+    test_time_points_to_consider = [1,2,3,4,5]
 
     # define the type of registration we'd like to use
-    registration_method = 'rigidfsl'
+    registration_method = 'nonlinearfsl'
+
+    # we want to normalise across all patients
+    inter_subject = True
 
     # initialise a preprocess pipeline based on the test subject
-    testProcess3DFLAIR = Process3DFLAIR(subject_info_df, test_subject_id, test_total_num_time_points, test_time_points_to_consider, registration_method, inter_subject = True) 
-
+    testProcess3DFLAIR = Process3DFLAIR(subject_info_df, test_subject_id, test_total_num_time_points, test_time_points_to_consider, registration_method, inter_subject) 
     
+    """
     # run variance pipeline
-    out_file = "/home/ela/Documents/B-RAPIDD/B-RAP_0027/3D-FLAIR/variance_maps/rigid/intersubnormalised_all_timepoints_affine.nii.gz"
+    out_file = "/home/ela/Documents/B-RAPIDD/B-RAP_0100/3D-FLAIR/variance_maps/nonlinear/intersubnormalised_all_timepoints_nonlinear.nii.gz"
    
     testProcess3DFLAIR.runVariancePipeline(out_file)
     #testProcess3DFLAIR.calcVariance(out_file) # use this if all of the preprocessing has already occurred
-
+    
      
-
+    """
+    
     """
     # run subtraction
-    out_file = "/home/ela/Documents/B-RAPIDD/B-RAP_0100/3D-FLAIR/rubbish.nii.gz"
+    out_file = "/home/ela/Documents/B-RAPIDD/B-RAP_0100/3D-FLAIR/subtraction_maps/nonlinear/2_minus_1.nii.gz"
 
     # minus non-ARIA from ARIA
-    in_image_num = 3
-    image_to_subtract_num = 1
-    #testProcess3DFLAIR.subtractImages(in_image_num, image_to_subtract_num, out_file, threshold = True)
-    testProcess3DFLAIR.runSubtraction(in_image_num, image_to_subtract_num, out_file)
-
+    in_image_num = 2 #3
+    image_to_subtract_num = 1 #1
+    testProcess3DFLAIR.subtractImages(in_image_num, image_to_subtract_num, out_file, threshold = False)
+    #testProcess3DFLAIR.runSubtraction(in_image_num, image_to_subtract_num, out_file)
+    
     """
+
+    # run gradient map calculation
+    out_folder = "/home/ela/Documents/B-RAPIDD/B-RAP_0100/3D-FLAIR/gradient_maps/"
+    days_between_scans = [0, 29, 58, 88, 118]
+    testProcess3DFLAIR.calcGradientMaps(out_folder, days_between_scans)
 
     # convert all of the temporal scans from DICOM to NIFTI
     #testProcess3DFLAIR.convertDICOMtoNIFTI() #NOTE: Edit this pipeline so that only files are converted if they don't exist
